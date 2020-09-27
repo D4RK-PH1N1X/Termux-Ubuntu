f5a161134bd85cb204252dae06a96598fac6db1f
D4RK PH1N1X <71398334+D4RK-PH1N1X@users.noreply.github.com>
2020-09-27 11:09:45 +0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -135,3 +135,6 @@ else
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;203m[ERROR]:\e[0m \x1b[38;5;87m Installation aborted.\n"
 printf "\e[0m"
 fi
+cp startubuntu.sh $HOME
+cp ubuntu-binds $HOME
+cp ubuntu-fs $HOME
